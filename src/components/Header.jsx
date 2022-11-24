80574a986263acc82a8cebc4f99f6024c5183636
wysiati <sachin.inguva@gmail.com>
2022-11-24 15:56:48 -0500

    ability to add items to cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,76 @@
 import React from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
-import { Navbar, Button, Alignment, Menu, MenuItem } from '@blueprintjs/core';
+import {
+  Navbar,
+  Button,
+  Alignment,
+  Menu,
+  MenuItem,
+  Card,
+  NonIdealState,
+} from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
 import { useAuth } from '../context/AuthContext';
+import { useCart } from '../context/CartContext';
 
 export function Header() {
   const auth = useAuth();
+  const { removeItemFromCart, cartItems } = useCart();
   const navigate = useNavigate();
+
+  const renderShoppingCart = () => {
+    if (!cartItems.length) {
+      return (
+        <NonIdealState
+          title="No Items Added to the cart"
+          icon="shopping-cart"
+        />
+      );
+    }
+    return (
+      <React.Fragment>
+        {cartItems.map(item => {
+          return (
+            <div
+              style={{
+                display: 'flex',
+                placeContent: 'center space-between',
+                alignItems: 'center',
+                padding: 10,
+              }}
+            >
+              <img
+                style={{
+                  height: 20,
+                  width: 20,
+                  marginRight: 10,
+                  borderRadius: 20,
+                }}
+                src={item.url}
+                alt={item.title}
+              />
+              <strong>{item.title}</strong>
+              <Button
+                minimal
+                intent="danger"
+                style={{ marginLeft: 10 }}
+                icon="delete"
+                onClick={() => removeItemFromCart(item)}
+              />
+            </div>
+          );
+        })}
+        <Button
+          fill
+          intent="primary"
+          text="Checkout"
+          rightIcon="arrow-right"
+          onClick={() => navigate('/checkout')}
+        />
+      </React.Fragment>
+    );
+  };
+
   return (
     <React.Fragment>
       <Navbar style={{ position: 'sticky', top: 0 }}>
@@ -24,12 +88,22 @@ export function Header() {
 
         {auth.user && (
           <Navbar.Group align={Alignment.RIGHT}>
-            <Button
-              minimal
-              intent="primary"
-              icon="home"
-              text="Home"
-              onClick={() => navigate('/')}
+            <Popover2
+              interactionKind="click"
+              placement="bottom"
+              content={
+                <Card style={{ borderRadius: 20 }}>{renderShoppingCart()}</Card>
+              }
+              renderTarget={({ isOpen, ref, ...targetProps }) => (
+                <Button
+                  {...targetProps}
+                  intent="primary"
+                  large
+                  elementRef={ref}
+                  minimal
+                  icon="shopping-cart"
+                />
+              )}
             />
             <Navbar.Divider />
             <Popover2
@@ -49,6 +123,8 @@ export function Header() {
                   {...targetProps}
                   elementRef={ref}
                   minimal
+                  intent="primary"
+                  large
                   icon="user"
                   rightIcon="chevron-down"
                 />
---
+++ src/context/CartContext.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+let CartContext = React.createContext(null);
+
+export function useCart() {
+  return React.useContext(CartContext);
+}
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = React.useState([]);
+
+  const addItemToCart = item => {
+    setCartItems(items => items.concat(item));
+  };
+
+  const removeItemFromCart = item => {
+    setCartItems(items => items.filter(_item => _item.url !== item.url));
+  };
+
+  const value = { cartItems, setCartItems, addItemToCart, removeItemFromCart };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+}
